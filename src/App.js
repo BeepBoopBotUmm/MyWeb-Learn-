c4114798e8206709bb060d19150f7acc929d8044
BeepBoopBotUmm <afifhuzaifah.07@gmail.com>
2022-06-26 20:09:26 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import React, { Component } from 'react';
-import { Routes, Route, useNavigate } from "react-router-dom"
+import { Routes, Route, useNavigate,useLocation } from "react-router-dom"
 import Home from './page/Home'
 import Welcome from './page/WelcomeSc'
 import About_me from './page/About_me';
 import Project from './page/Project';
+import Potraitmode from './page/Potraitmode'
+import Nav from './SrcFuncCode/Nav'
 
 function App() {
+  let navigate = useNavigate()
+  let StandLocation = ''
+  let AlrChange = false
+  const location = useLocation()
+  let portrait = window.matchMedia("(orientation: portrait)");
+  portrait.addEventListener("change", function(e) {
+    if(e.matches) {
+      Nav(navigate,'/Potrait')
+      console.log('Change to potrait')
+      StandLocation = location.pathname.replace('/MyWeb-Learn-','')
+      AlrChange = true
+    } else {
+      console.log('in landscape')
+      if (AlrChange === true){
+        Nav(navigate,StandLocation)
+      }
+    }
+  })
   return (
     <>
       <Routes>
@@ -14,6 +34,7 @@ function App() {
         <Route path='/MyWeb-Learn-/Home' element={<Home/>} />
         <Route path='/MyWeb-Learn-/About' element={<About_me />}/>
         <Route path='/MyWeb-Learn-/Project' element={<Project />}/>
+        <Route path='/MyWeb-Learn-/Potrait' element={<Potraitmode/>}/>
       </Routes>
     </>
   );
---
+++ src/page/Potraitmode.js
@@ -0,0 +1,19 @@
+import './page_css/Potraitmode.css';
+import React, { Component } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import AddStyle from '../SrcFuncCode/AddStyle'
+import Nav from '../SrcFuncCode/Nav'
+
+function Potraitmode(){
+    return(
+        <>
+            <body>
+                <section className='Info'>
+                    <h1>Rotate your phone</h1>
+                </section>
+            </body>
+        </>
+    )
+}
+
+export default Potraitmode
