a9a619ff73cfdf62592a1ea2391dd65016b2804f
BeepBoopBotUmm <afifhuzaifah.07@gmail.com>
2022-06-26 17:50:46 +0800

    f

--- src/SrcFuncCode/Nav.js
+++ src/SrcFuncCode/Nav.js
@@ -9,7 +9,7 @@ async function Nav(navigate,Direct){
         if (Direct.charAt(0) === '/'){
             console.log('pass 2')
             let NewDir = MainLink + Direct
-            //console.log(NewDir)
+            console.log(NewDir)
             navigate(NewDir)
         }else{
             console.warn('Error Invalid Link')
