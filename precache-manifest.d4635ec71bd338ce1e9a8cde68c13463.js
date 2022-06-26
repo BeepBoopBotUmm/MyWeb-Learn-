9377dd392f7406056fedb5c54f79c9d1dff3b938
BeepBoopBotUmm <afifhuzaifah.07@gmail.com>
2022-06-26 15:35:59 +0800

    Updates

---
+++ precache-manifest.d4635ec71bd338ce1e9a8cde68c13463.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "140d77dd83e065f3ce5b",
+    "url": "/Myweb/static/css/main.2a85833c.chunk.css"
+  },
+  {
+    "revision": "140d77dd83e065f3ce5b",
+    "url": "/Myweb/static/js/main.140d77dd.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/Myweb/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "22c31a72ed835e4963f4",
+    "url": "/Myweb/static/js/2.22c31a72.chunk.js"
+  },
+  {
+    "revision": "a9c1ae4377b3bb359a1a",
+    "url": "/Myweb/static/js/runtime~main.a9c1ae43.js"
+  },
+  {
+    "revision": "34ada1638ef7bef370e593e45e2cd29a",
+    "url": "/Myweb/static/media/Instagram_Logo.34ada163.png"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/Myweb/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/Myweb/static/media/futura-medium-bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "3d74e8bfd4ef7985f7529bb9f7650eca",
+    "url": "/Myweb/static/media/Twitter Logo.3d74e8bf.png"
+  },
+  {
+    "revision": "e92afe0bc37e1ae51f9f75d9c2a21bfb",
+    "url": "/Myweb/static/media/DiscordLogo.e92afe0b.png"
+  },
+  {
+    "revision": "ab9723750e15cff087f5e8033c3685fd",
+    "url": "/Myweb/static/media/Roblox Logo.ab972375.png"
+  },
+  {
+    "revision": "907e2fae5eac00f2d36abee2e1821121",
+    "url": "/Myweb/index.html"
+  }
+];
